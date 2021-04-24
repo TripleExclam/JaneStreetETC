f35967e05a89e80a43a7ea9cdb2498a1e3b49c50
TripleExclam <burtonmatthew00@gmail.com>
2021-04-24 11:51:08 +1000

    123

--- parser.py
+++ parser.py
@@ -4,5 +4,5 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class SimpleExample:
-
+    type: str
 
--- sample-bot.py
+++ sample-bot.py
@@ -12,7 +12,7 @@ import json
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
-team_name = "REPLACEME"
+team_name = "BUYHIGHSELLLOW"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
 test_mode = True
